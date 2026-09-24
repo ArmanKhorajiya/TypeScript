0b28913f783bf8956eef1df25c06364d75d07db7
Arman <armankhorajiyask@gmail.com>
2026-09-24 17:13:41 +0530

    TS

---
+++ src/TypeScript_Assignment_Food_Order_System/bank-account.ts
@@ -0,0 +1,37 @@
+// 1. Class
+
+// A class is a blueprint for creating objects.
+
+// class BankAccount {
+//     accountNumber: number;
+//     accountHolder: string;
+//     balance: number;
+//     accountType: string;
+// }
+// 2. Object
+
+// An object is created from the class:
+
+// const account1 = new BankAccount();
+
+// Now account1 is a BankAccount object.
+
+// 3. Try this now
+
+// Put this in src/bank-account.ts:
+
+// class BankAccount {
+//     accountNumber: number;
+//     accountHolder: string;
+//     balance: number;
+//     accountType: string;
+// }
+
+// const account1 = new BankAccount();
+
+// account1.accountNumber = 101;
+// account1.accountHolder = "Satya";
+// account1.balance = 10000;
+// account1.accountType = "Savings";
+
+// console.log(account1);
