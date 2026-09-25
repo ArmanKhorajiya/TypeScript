b0d6e52ef8fef302c42c64ef0850a9f53f20a9f6
Arman <armankhorajiyask@gmail.com>
2026-09-25 15:12:17 +0530

    Update

--- src/TypeScript_Assignment_Food_Order_System/bank-account.ts
+++ src/TypeScript_Assignment_Food_Order_System/bank-account.ts
@@ -1,22 +1,63 @@
-class BankAccount {
-  accountNumber: number;
-  accountHolder: string;
-  balance: number;
-  accountType: string;
+// class BankAccount {
+//   readonly accountNumber: number;
+//   public accountHolder: string;
+//   private balance: number;
+//   protected accountType: string;
 
-  constructor(
-    accountNUmber: number,
-    accountHolder: string,
-    balance: number,
-    accountType: string,
-  ) {
-    this.accountNumber = accountNUmber;
-    this.accountHolder = accountHolder;
-    this.balance = balance;
-    this.accountType = accountType;
+//   constructor(
+//     accountNUmber: number,
+//     accountHolder: string,
+//     balance: number,
+//     accountType: string,
+//   ) {
+//     this.accountNumber = accountNUmber;
+//     this.accountHolder = accountHolder;
+//     this.balance = balance;
+//     this.accountType = accountType;
+//   }
+// }
+
+// const account1 = new BankAccount(101, "Arman", 10000, "Savings");
+// console.log(account1);
+
+// class Bank {
+//   static totalAccounts = 0;
+
+//   constructor() {
+//     Bank.totalAccounts += 1;
+//   }
+
+//   static getTotalAccounts() {
+//     return Bank.totalAccounts;
+//   }
+// }
+
+// const b1 = new Bank();
+// const b2 = new Bank();
+// const b3 = new Bank();
+
+// console.log(Bank.getTotalAccounts());
+
+class Student {
+  private name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  set studentName(name: string) {
+    if (name.length < 3) {
+      throw new Error("Name must have at least 3 characyers");
+    }
+    this.name = name.toUpperCase();
+  }
+
+  get studentName() {
+    return this.name;
   }
 }
+const student = new Student("arman");
 
-const account1 = new BankAccount(101, "Arman", 10000, "Savings");
-console.log(account1);
+student.studentName = "arman";
 
+console.log(student.studentName);
